1d2249b5519dee149d0e2b54c0526eeefe33ba06
ipa-black <ccf17ih@gmail.com>
2026-05-13 16:36:03 +0300

    Update TabbarView.swift

--- Feather/Views/TabView/Bars/TabbarView.swift
+++ Feather/Views/TabView/Bars/TabbarView.swift
@@ -1,18 +1,19 @@
 //
 //  TabbarView.swift
-//  feather
+//  SY STORE
 //
 //  Created by samara on 23.03.2025.
+//  Modified for SY STORE.
 //
 
 import SwiftUI
 
 struct TabbarView: View {
-	@State private var selectedTab: TabEnum = .sources
+	@State private var selectedTab: TabEnum = .home // تم التعديل لتصبح الرئيسية هي الافتراضية
 
 	var body: some View {
 		TabView(selection: $selectedTab) {
-			ForEach(TabEnum.defaultTabs, id: \.hashValue) { tab in
+			ForEach(TabEnum.defaultTabs, id: \.self) { tab in // تم تغيير .hashValue إلى .self لحل الخطأ
 				TabEnum.view(for: tab)
 					.tabItem {
 						Label(tab.title, systemImage: tab.icon)
